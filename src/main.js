40ccc215ab5499b41f784c2fd9ec98d3b27bbc09
Erin Ringland <erinringland@gmail.com>
2020-12-16 13:35:14 -0600

    Added more features to CSS

--- src/main.js
+++ src/main.js
@@ -1,22 +1,30 @@
 const moviesList = document.getElementById('movies');
 
-fetch('/star_wars.json')
-  .then(response => response.json())
-  .then(movies => showMovies(movies.Search));
-
 function showMovies(movies) {
   console.log(movies);
-  let descriptions = movies.map(movie => movie.imdbID).map(id => fetch('/new_hope.json').then(response => response.json()));
-  Promise.all(descriptions).then(descriptions => console.log(descriptions));
   moviesList.innerHTML = movies
     .map(function (movie) {
       return `<li>
       <img src="${movie.Poster}">
       <div class="info">
         <div class="title">${movie.Title}</div>
-        <div class="year">${movie.Year}</div>
+        <div class="rating">${movie.Ratings[0].Value}</div>
+        <div class="plot">${movie.Plot}</div>
       </div>
       </li>`;
     })
     .join('');
 }
+
+function getMovies(search = 'star wars') {
+  fetch('/star_wars.json')
+    .then(response => response.json())
+    .then(movies => {
+      let descriptions = movies.Search
+        .map(movie => movie.imdbID)
+        .map(id => fetch('/new_hope.json').then(response => response.json()));
+      Promise.all(descriptions).then(descriptions => showMovies(descriptions));      
+    });
+}
+
+getMovies();
