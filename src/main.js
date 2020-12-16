bab265b84bd40a8a8935705817b99335c58cbac3
Erin Ringland <erinringland@gmail.com>
2020-12-16 10:45:56 -0600

    Displaying the results

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
+const moviesList = document.getElementById('movies');
+
 fetch('/star_wars.json')
   .then(response => response.json())
-  .then(movies => console.log(movies.Search));
+  .then(movies => showMovies(movies.Search));
 
+function showMovies(movies) {
+  console.log(movies);
+  moviesList.innerHTML = movies
+    .map(function (movie) {
+      return `<li>${movie.Title}</li>`;
+    })
+    .join('');
+}
